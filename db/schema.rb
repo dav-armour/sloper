b6ceabc974e24c6b7f3e1f419ac4e8e356a20288
ken <kenshusrs@gmail.com>
2018-10-30 16:30:41 +1100

    bucket changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_10_30_000747) do
+ActiveRecord::Schema.define(version: 2018_10_30_031347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
