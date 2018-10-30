f2b0ccb7a63d70bc71ec3a9b9c99ac3a42a97aec
Luigio De Leon <ginodleon@gmail.com>
2018-10-30 16:32:48 +1100

    fix dependencies when destroying a listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,10 +1,10 @@
 class Listing < ApplicationRecord
   belongs_to :user
-  belongs_to :location
-  has_many :images
-  has_many :bookings
+  has_one :location, dependent: :destroy
+  has_many :images, dependent: :destroy
+  has_many :bookings, dependent: :destroy
   has_many :renters, through: :bookings
-  has_many :available_days
+  has_many :available_days, dependent: :destroy
   enum item_type: [:Freestyle, :All_Mountain, :Freeride, :Powder, :Carving, :Other]
   enum category: [:Snowboard, :Ski]
 end
