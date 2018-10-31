c131878b9b3bce92962c37104934f5c17f69af0d
David Armour <15181647+dav-armour@users.noreply.github.com>
2018-11-01 08:40:17 +1100

    fix bugs in listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -26,19 +26,20 @@ class ListingsController < ApplicationController
   # POST /listings
   # POST /listings.json
   def create
-    @listing = Listing.new(listing_params)
-    @listing.user_id = current_user.id
-    if params[:listing][:listing_image]
-      params[:listing][:listing_image][:image].each do |img|
-        @image = @listing.listing_images.create(image: img)
+    begin
+      @listing = Listing.new(listing_params)
+      @listing.user_id = current_user.id
+      listing_saved = @listing.save
+      raise "Couldn't save listing." unless listing_saved
+      if params[:listing][:listing_image] && listing_saved
+        params[:listing][:listing_image][:image].each do |img|
+          @image = @listing.listing_images.create(image: img)
+          raise "Couldn't create image. #{img}" unless @image
+        end
       end
-    end
-    respond_to do |format|
-      if @listing.save
-        format.html { redirect_to @listing, notice: 'Listing was successfully created.' }
-      else
-        format.html { render :new }
-      end
+      redirect_to @listing, notice: 'Listing was successfully created.'
+    rescue StandardError => e
+      redirect_to new_listing_path(@listing), alert: e.message
     end
   end
 
