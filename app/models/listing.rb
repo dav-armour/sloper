f65e2ffd603aed95f619d893e1a1955633269e2d
ken <kenshusrs@gmail.com>
2018-11-05 15:46:12 +1100

    update devise form fields

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -11,7 +11,6 @@ class Listing < ApplicationRecord
   enum category: [:Snowboard, :Ski]
   validates_associated :location
   validates :title, :category, :item_type, :brand, :size, :daily_price, :weekly_price, presence: { message: "must be given." }
-
   validates :size, inclusion: { in: 90..220,
     message: "%{value}cm is not within the accepted range of 90cm to 200cm" }
   validates :daily_price, :weekly_price, numericality: { greater_than: 0 }
