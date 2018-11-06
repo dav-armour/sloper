3ca8358f24e68e2f592b59b74a52cba9fbca9386
Luigino De Leon <ginodleon@gmail.com>
2018-11-06 13:44:55 +1100

    add bootstrap cards to the listings page

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,12 @@
 class Location < ApplicationRecord
   before_save :remove_whitespace
   belongs_to :listing
+
+  def remove_whitespace
+    attribute_names.each do |name|
+      if send(name).respond_to?(:strip)
+        send("#{name}=", send(name).strip)
+      end
+    end
+  end
 end
