38622b27b3518f489000fd257af21c792d80d1ba
ken <kenshusrs@gmail.com>
2018-11-08 14:08:42 +1100

    carousel centering

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -29,8 +29,8 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [500, 500]
-    process crop: '500x500+0+0'
+    process resize_to_fit: [338, 253]
+    # process crop: '500x500+0+0'
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
